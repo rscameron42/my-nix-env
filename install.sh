cdd701726eac5783b4253d3b2373277b937ff4fb
rscameron42 <47338215+rscameron42@users.noreply.github.com>
2021-01-29 14:58:00 -0800

    Fixed differences with Linux abd BSD find commands in install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Find all dot files then if the original file exists, create a backup
-# Once backed up to {file}.dtbak symlink the new dotfile in place
-for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
+# Once backed up to {file}.mnebak symlink the new dotfile in place
+#for file in $(find . -maxdepth 1 -name ".*" -type f  -printf "%f\n" ); do
+for file in $(find . -maxdepth 1 -name ".*" -type f  | sed s/".\/"// ); do
     if [ -e ~/$file ]; then
         mv -f ~/$file{,.mnebak}
     fi
